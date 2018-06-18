8c9ef774dc1bb343147ee0d1849b468707a7fc02
Deadpikle <mikebobby@sbcglobal.net>
2018-06-18 16:05:00 -0400

    Initial Commit

---
+++ TestCast/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestCast
+//
+//  Created by School of Computing Macbook on 6/18/18.
+//  Copyright © 2018 Pikle Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
